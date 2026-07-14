42ed0691af246fdd516d18dd359bd2ea512e4668
Craig Slusher <cslush@gmail.com>
2026-07-13 22:03:36 -0400

    fix: pass score.evaluator.model and provider to HarnessEvaluator
    Closes #31

--- packages/core/src/hall/concert-hall.ts
+++ packages/core/src/hall/concert-hall.ts
@@ -83,6 +83,8 @@ export class ConcertHall implements ChildConcertFactory {
       return new HarnessEvaluator({
         adapter,
         promptTemplate: score.evaluator?.prompt,
+        model: score.evaluator?.model,
+        provider: score.evaluator?.provider,
       });
     }
     return this.evaluator;
